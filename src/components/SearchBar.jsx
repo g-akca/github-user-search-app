efffe0331e1b3af5d60de12c2e5052af06a4038a
g-akca <gok.akca66@gmail.com>
2026-05-15 21:46:07 +0300

    Add SearchBar content and styles

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,21 @@
+import searchIcon from "/images/icon-search.svg";
+
 function SearchBar() {
   return (
-    <>
-    </>
+    <form className="bg-white px-3 py-2 h-17.25 rounded-2xl shadow-[0_16px_30px_-10px_rgba(70,96,187,0.2)] flex justify-between items-center gap-1">
+      <div className="grow flex gap-2 items-center">
+        <img src={searchIcon} alt="Search icon" className="w-5" />
+
+        <input 
+          className="grow text-[13px] leading-[140%] text-neutral-700 placeholder:text-neutral-500" 
+          placeholder="Search GitHub username…" 
+        />
+      </div>
+
+      <button type="submit" className="h-12 px-5 py-3 bg-blue-500 rounded-[10px] text-white text-[16px] leading-base font-bold">
+        Search
+      </button>
+    </form>
   )
 }
 
