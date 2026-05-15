7392850b3d5ee9bc7d0954117331c53f47afd031
g-akca <gok.akca66@gmail.com>
2026-05-15 21:32:48 +0300

    Add ThemeSwitcher content and styles

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,7 +1,11 @@
+import moonIcon from "/images/icon-moon.svg";
+
 function ThemeSwitcher() {
   return (
-    <>
-    </>
+    <button type="button" className="flex items-center gap-3">
+      <p className="uppercase text-[13px] leading-[140%] font-bold tracking-[2.5px]">Dark</p>
+      <img src={moonIcon} alt="Moon icon" className="w-5 aspect-square" />
+    </button>
   )
 }
 
