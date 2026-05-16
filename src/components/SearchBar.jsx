884e102c3f6b572e91280fb49373abec31929a08
g-akca <gok.akca66@gmail.com>
2026-05-16 21:40:22 +0300

    Create state for searched value

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import searchIcon from "/images/icon-search.svg";
 
 function SearchBar() {
+  const [searchedValue, setSearchedValue] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <form className="bg-white px-3 py-2 h-17.25 rounded-2xl shadow-[0_16px_30px_-10px_rgba(70,96,187,0.2)] flex justify-between items-center gap-1">
+    <form onSubmit={handleSubmit} className="bg-white px-3 py-2 h-17.25 rounded-2xl shadow-[0_16px_30px_-10px_rgba(70,96,187,0.2)] flex justify-between items-center gap-1">
       <div className="grow flex gap-2 items-center">
         <img src={searchIcon} alt="Search icon" className="w-5" />
 
         <input 
           className="grow text-[13px] leading-[140%] text-neutral-700 placeholder:text-neutral-500" 
           placeholder="Search GitHub username…" 
+          value={searchedValue}
+          onChange={(e) => setSearchedValue(e.target.value)}
         />
       </div>
 
