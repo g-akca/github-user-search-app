7ee250e4f2dbfdc5ff290afc2db952039f54f44f
g-akca <gok.akca66@gmail.com>
2026-05-16 15:55:58 +0300

    Give ProfileHeader content and styles

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,7 +1,13 @@
 function ProfileHeader() {
   return (
-    <>
-    </>
+    <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-0.5">
+        <h2 className="text-[26px] leading-[120%] font-bold text-neutral-700">The Octocat</h2>
+        <p className="text-[16px] leading-base text-blue-500">@octocat</p>
+      </div>
+
+      <p>Joined 25 Jan 2011</p>
+    </div>
   )
 }
 
