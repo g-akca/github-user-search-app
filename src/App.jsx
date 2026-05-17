ccdc5c7d623e4480f51fb61af9d6065018df391c
g-akca <gok.akca66@gmail.com>
2026-05-17 23:35:12 +0300

    Add tablet responsive App styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ function App() {
   const [loading, setLoading] = useState(false);
 
   return (
-    <div className="text-base leading-base text-neutral-500 min-h-screen bg-neutral-100 px-4 py-8 flex justify-center">
-      <div className="grow flex flex-col gap-8 max-w-182.5">
+    <div className="text-base leading-base text-neutral-500 min-h-screen bg-neutral-100 px-4 py-8 flex justify-center tablet:px-8 tablet:py-10">
+      <div className="grow flex flex-col gap-8 max-w-182.5 tablet:gap-10">
         <Header />
+        
         <SearchBar setData={setData} setError={setError} setLoading={setLoading} error={error} />
 
         {!loading ? (
