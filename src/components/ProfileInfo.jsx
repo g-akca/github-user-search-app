4f452b6606c5964d34e50da88d6edbc5f4ccfb13
g-akca <gok.akca66@gmail.com>
2026-05-18 00:29:42 +0300

    Add tablet responsive ProfileInfo styles

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -8,7 +8,7 @@ function ProfileInfo({ data }) {
     <div className="flex flex-col gap-6">
       <p className={data.bio || "opacity-75"}>{data.bio ? data.bio : "This profile has no bio"}</p>
 
-      <div className="bg-neutral-100 rounded-[10px] py-4 px-5 flex flex-col gap-4">
+      <div className="bg-neutral-100 rounded-[10px] py-4 px-5 flex flex-col gap-4 tablet:px-8 tablet:grid tablet:grid-cols-3">
         <div className="flex flex-col gap-1">
           <p className="text-[13px] leading-base">Repos</p>
           <p className="text-neutral-700 font-bold text-[22px] leading-[140%]">{data.public_repos}</p>
@@ -25,7 +25,7 @@ function ProfileInfo({ data }) {
         </div>
       </div>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 tablet:grid tablet:grid-cols-2">
         <div className={`flex items-center gap-4 ${data.location || "opacity-70"}`}>
           <img src={locationIcon} alt="Location icon" />
           <p>{data.location ? data.location : "Not Available"}</p>
