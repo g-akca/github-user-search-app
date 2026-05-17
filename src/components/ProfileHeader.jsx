cc3632a19db724204687a0b3bdbecc52384fdc56
g-akca <gok.akca66@gmail.com>
2026-05-17 12:41:07 +0300

    Use data in ProfileHeader

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,12 +1,18 @@
-function ProfileHeader() {
+function ProfileHeader({ data }) {
   return (
     <div className="flex flex-col gap-1">
       <div className="flex flex-col gap-0.5">
-        <h2 className="text-[26px] leading-[120%] font-bold text-neutral-700">The Octocat</h2>
-        <p className="text-[16px] leading-base text-blue-500">@octocat</p>
+        <h2 className="text-[26px] leading-[120%] font-bold text-neutral-700">{data.name}</h2>
+        <p className="text-[16px] leading-base text-blue-500">@{data.login}</p>
       </div>
 
-      <p>Joined 25 Jan 2011</p>
+      <p>
+        Joined {new Date(data.created_at).toLocaleDateString("en-GB", {
+          day: "2-digit",
+          month: "short",
+          year: "numeric"
+        })}
+      </p>
     </div>
   )
 }
