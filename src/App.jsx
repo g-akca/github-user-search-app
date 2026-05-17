e981f9633d860987bc4ad3a3a803d58994f1dc55
g-akca <gok.akca66@gmail.com>
2026-05-17 19:16:56 +0300

    Create loading state, show loading text

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,19 @@ import { useState } from "react";
 function App() {
   const [data, setData] = useState({});
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className="text-base leading-base text-neutral-500 min-h-screen bg-neutral-100 px-4 py-8 flex justify-center">
       <div className="grow flex flex-col gap-8 max-w-182.5">
         <Header />
-        <SearchBar setData={setData} setError={setError} />
-        <ProfileSection data={data} />
+        <SearchBar setData={setData} setError={setError} setLoading={setLoading} />
+
+        {!loading ? (
+          <ProfileSection data={data} />
+        ) : (
+          <p className="text-center">Loading...</p>
+        )}
       </div>
     </div>
   )
