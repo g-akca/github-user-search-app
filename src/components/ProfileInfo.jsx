3f146705a54bba139f317b3870a01d6618c59715
g-akca <gok.akca66@gmail.com>
2026-05-17 19:13:42 +0300

    Use data in ProfileInfo

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -3,47 +3,47 @@ import twitterIcon from "/images/icon-twitter.svg";
 import websiteIcon from "/images/icon-website.svg";
 import companyIcon from "/images/icon-company.svg";
 
-function ProfileInfo() {
+function ProfileInfo({ data }) {
   return (
     <div className="flex flex-col gap-6">
-      <p className="opacity-75">This profile has no bio</p>
+      <p className="opacity-75">{data.bio ? data.bio : "This profile has no bio"}</p>
 
       <div className="bg-neutral-100 rounded-[10px] py-4 px-5 flex flex-col gap-4">
         <div className="flex flex-col gap-1">
           <p className="text-[13px] leading-base">Repos</p>
-          <p className="text-neutral-700 font-bold text-[22px] leading-[140%]">8</p>
+          <p className="text-neutral-700 font-bold text-[22px] leading-[140%]">{data.public_repos}</p>
         </div>
 
         <div className="flex flex-col gap-1">
           <p className="text-[13px] leading-base">Followers</p>
-          <p className="text-neutral-700 font-bold text-[22px] leading-[140%]">3938</p>
+          <p className="text-neutral-700 font-bold text-[22px] leading-[140%]">{data.followers}</p>
         </div>
 
         <div className="flex flex-col gap-1">
           <p className="text-[13px] leading-base">Following</p>
-          <p className="text-neutral-700 font-bold text-[22px] leading-[140%]">9</p>
+          <p className="text-neutral-700 font-bold text-[22px] leading-[140%]">{data.following}</p>
         </div>
       </div>
 
       <div className="flex flex-col gap-4">
         <div className="flex items-center gap-4">
           <img src={locationIcon} alt="Location icon" />
-          <p>San Francisco</p>
+          <p>{data.location ? data.location : "Not Available"}</p>
         </div>
 
         <div className="flex items-center gap-4 opacity-70">
           <img src={twitterIcon} alt="Twitter icon" />
-          <p>Not Available</p>
+          <p>{data.twitter_username ? data.twitter_username : "Not Available"}</p>
         </div>
 
         <div className="flex items-center gap-4">
           <img src={websiteIcon} alt="Website icon" />
-          <p>https://github.blog</p>
+          <p>{data.blog ? data.blog : "Not Available"}</p>
         </div>
 
         <div className="flex items-center gap-4">
           <img src={companyIcon} alt="Company icon" />
-          <p>@github</p>
+          <p>{data.company ? data.company : "Not Available"}</p>
         </div>
       </div>
     </div>
