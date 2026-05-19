29708bf1b76c2cbecab94407550f1a7d3152c053
g-akca <gok.akca66@gmail.com>
2026-05-19 22:37:27 +0300

    Add dark mode ProfileSection styles

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -3,7 +3,12 @@ import ProfileInfo from "./ProfileInfo";
 
 function ProfileSection({ data, error }) {
   return (
-    <main className="bg-white rounded-[15px] shadow-[0_16px_30px_-10px_rgba(70,96,187,0.2)] px-6 py-8 flex flex-col gap-6 tablet:px-8 tablet:py-12 tablet:flex-row tablet:gap-8 desktop:p-12">
+    <main 
+      className="
+        bg-white dark:bg-neutral-800 rounded-[15px] shadow-[0_16px_30px_-10px_rgba(70,96,187,0.2)] dark:shadow-none 
+        px-6 py-8 flex flex-col gap-6 tablet:px-8 tablet:py-12 tablet:flex-row tablet:gap-8 desktop:p-12
+      "
+    >
       {!error ? (
         <>
           <div className="flex gap-5 items-start">
@@ -20,9 +25,9 @@ function ProfileSection({ data, error }) {
         </>
       ) : (
         <div className="flex flex-col gap-4 text-center">
-          <h2 className="text-neutral-700 text-[22px] leading-[140%] font-bold">No results found!</h2>
+          <h2 className="text-neutral-700 dark:text-white text-[22px] leading-[140%] font-bold">No results found!</h2>
 
-          <p className="text-neutral-300">We couldn’t find any GitHub users matching your search. Please double-check the username and try again.</p>
+          <p className="text-neutral-300 dark:text-neutral-200">We couldn’t find any GitHub users matching your search. Please double-check the username and try again.</p>
         </div>
       )}
     </main>
