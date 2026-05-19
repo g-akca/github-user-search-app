ba3126ba6513e3226d41142abbb8af8ac8b81659
g-akca <gok.akca66@gmail.com>
2026-05-19 12:47:30 +0300

    Create ThemeContext for dark mode state

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,17 @@
+const { createContext, useContext, useState } = require("react");
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
