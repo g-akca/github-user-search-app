6aea6a0bc602835b0a3339343dfeb60465e2dae3
g-akca <gok.akca66@gmail.com>
2026-05-20 17:44:01 +0300

    Add dark mode hover and focus styles

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -12,7 +12,7 @@ function ThemeSwitcher() {
       className="
         flex items-center gap-3 cursor-pointer transition-colors outline-blue-500 tablet:gap-4 hover:text-neutral-700
         focus:rounded-sm focus:outline-offset-[3px] focus:outline-2 focus:outline-blue-500 focus:text-neutral-700
-        dark:text-neutral-200
+        dark:text-neutral-200 dark:hover:text-white dark:focus:text-white
       "
     >
       <p className="uppercase text-[13px] leading-[140%] font-bold tracking-[2.5px]">
